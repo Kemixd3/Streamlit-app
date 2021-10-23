89ac081a131dedfd957ebc6d54292331f1aaf1f1
Kemixd3 <81220926+Kemixd3@users.noreply.github.com>
2021-10-24 01:36:04 +0200

    Update webs.py

--- webs.py
+++ webs.py
@@ -12,7 +12,7 @@ from streamlit_autorefresh import st_autorefresh
 import altair as alt
 #import back as fl
 os.system('"dist.tost.py"')
-from /app/streamlit-app/dist/tost import g
+from dist.tost import g
 
 
 #st.set_page_config
