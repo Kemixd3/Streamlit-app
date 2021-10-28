0be367e8daa9afa418a39cbf0765ef9ab0e4b49e
Kemixd3 <81220926+Kemixd3@users.noreply.github.com>
2021-10-28 21:31:17 +0200

    Update usessss.py

--- kryp/usessss.py
+++ kryp/usessss.py
@@ -3,7 +3,7 @@ import os
 #from pytransform import pyarmor_runtime
 #pyarmor_runtime()
 from pytransform import _load_library
-m = _load_library(path='/path/to/dist')
+m = _load_library(path='dist/pytransform')
 m()
 import streamlit as st
 from pytransform import pyarmor_runtime
