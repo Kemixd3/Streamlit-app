a658b970554216444bcb36e3cb71e83dc39147b5
Kemixd3 <81220926+Kemixd3@users.noreply.github.com>
2021-10-28 16:20:56 +0200

    Update webs.py

--- webs.py
+++ webs.py
@@ -26,6 +26,15 @@ import kryp.usessss as fh
 
 #st.set_page_config
 
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
 st.set_page_config(
     page_title="Really cool app",
     page_icon="random",
